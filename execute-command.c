8ccaf3b4e28c284119e9dc633d8dd1c4db370b78
charlysotelo <charlysotelo@hotmail.com>
2013-10-18 02:47:44 -0700

    Update execute-command.c

--- execute-command.c
+++ execute-command.c
@@ -7,8 +7,7 @@
 #include <error.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-
-#include <stdio.h>
+#include "stdio.h"
 
 #define UNUSED(x) (void) (x);
 
@@ -37,8 +36,10 @@ execute_command (command_t c, bool time_travel)
       execute_simple_command(c);
       break;
     case SEQUENCE_COMMAND:
+      execute_sequence_command(c);
       break;
     case PIPE_COMMAND:
+      execute_pipe_command(c);
       break;
     case SUBSHELL_COMMAND:
       execute_subshell_command(c);
@@ -46,8 +47,40 @@ execute_command (command_t c, bool time_travel)
   }
 }
 
-int
-execute_pipe_command(command_t command) //a|b
+int execute_sequence_command(command_t command)
+{
+  int status;
+  pid_t p;
+
+  if( (p = fork()) < 0)
+    error(1,0,"Fork in sequence_command failed");
+
+  if(p == 0)
+  {
+    execute_command(command->u.command[0],false);
+    _exit(command->u.command[0]->status); 
+  }
+  else
+  {
+    execute_command(command->u.command[1],false);
+    
+    if(waitpid(p,&status,0) < 0)
+      error(1,0,"Waitpid in sequence_command failed");
+
+    command->status = command->u.command[1]->status;
+  }
+  
+  return command->status;
+}
+
+int execute_subshell_command(command_t command)
+{
+  execute_command(command->u.subshell_command,false);
+  command->status = command->u.subshell_command->status;
+  return command->status;
+}
+
+int execute_pipe_command(command_t command) //a|b
 {
   int fd[2];
   int status, status2;
@@ -98,7 +131,7 @@ execute_pipe_command(command_t command) //a|b
     }
   }
   else //wait for child process to return
-  {
+  { 
     if(waitpid(p,&status,0) < 0)
       error(1,0,"Outermost grandparent wait failed");
     //printf("I am the father and I waited forsons\n");
@@ -106,82 +139,81 @@ execute_pipe_command(command_t command) //a|b
   } 
 }
 
-pid_t execute_and_or_command(command_t command)
+int execute_and_or_command(command_t command)
 {
-  pid_t pid;
+  pid_t p;
   int status;
   command_t left = command->u.command[0];
   command_t right = command->u.command[1];
 
-  pid = fork();
-  if (pid == 0) {
-    execute_command(left, false);
+  p = fork();
+  if( p == 0 )
+  {
+    execute_command(left,false);
     _exit(left->status);
-  } else if (left->type == SIMPLE_COMMAND) {
-    waitpid(pid, &status, 0);
-    if (!WIFEXITED(status)) {
-      error(1, 0, "Child never exited.\n");
-    }
-    left->status = WEXITSTATUS(status);
   }
-
-  if((command->type == AND_COMMAND && left->status == 0) ||
-    (command->type == OR_COMMAND && left->status != 0)) { // left not/successful, execute right side
-    pid = fork();
-    if (pid == 0) {
-      execute_command(right, false);
-      _exit(right->status);
-    } else if (right->type == SIMPLE_COMMAND) {
-      waitpid(pid, &status, 0);
-      if (!WIFEXITED(status)) {
-        error(1, 0, "Child never exited.\n");
-      }
-      right->status = WEXITSTATUS(status);
+  else
+  {
+    //why fork and wait immediately? Why not just use the parent process to do everything?
+    if(waitpid(p,&status,0) < 0)
+      error(1,0,"Waitpid on and/or failed");
+  
+    if( (command->type == AND_COMMAND && left->status == 0) ||
+        (command->type == OR_COMMAND  && left->status != 0))
+    {
+      execute_command(right,false);
+      command->status = right->status;
     }
-    command->status = right->status;
   }
-
-  return pid;
-}
-
-pid_t execute_subshell_command(command_t command)
-{
-  pid_t pid;
-  int status;
-
-  // SEGMENTATION FAULT!!!
-  command_t subshell = command->u.subshell_command;
-
-  printf("Trying to execute subshell command %s\n", subshell->u.word[0]);
-  pid = fork();
-  if (pid == 0) {
-    execute_command(subshell, false);
-    _exit(subshell->status);
-  } else {
-    waitpid(pid, &status, 0);
-    if (!WIFEXITED(status)) {
-      error(1, 0, "Child never exited.\n");
-    }
-    command->status = WEXITSTATUS(status);
-  }
-
-  return pid;
+  return command->status;
 }
 
 int execute_simple_command(command_t command)
 {
   int status;
-  pid_t pid = fork();
-  if (pid == 0) {
-    execvp(command->u.word[0], command->u.word);
-    _exit(1);
-  } else {
-    waitpid(pid, &status, 0);
-    if (!WIFEXITED(status)) {
-      error(1, 0, "Child never exited.\n");
+
+  //TODO: check for redirections!
+  pid_t p = fork();
+  if (p == 0) 
+  {
+    int inputFile, outputFile;
+    FILE * inStream = NULL;
+    FILE * outStream = NULL;
+    if( command->input != NULL )
+    {
+      if( (inStream = fopen(command->input,"r")) == NULL )
+        error(1,0,"Input file not found: %s",command->input);
+      
+      inputFile = fileno(inStream);
+      dup2(inputFile,0);
+      //should i close the file descriptor here? will it work?
+      if(inputFile != 0 && inputFile != 1 && inputFile != 2)
+        close(inputFile);
     }
-    command->status = WEXITSTATUS(status);
+    
+    if( command->output != NULL )
+    {
+      if( (outStream = fopen(command->output,"w")) == NULL )
+        error(1,0,"Output file not found: %s",command->output);
+
+      outputFile = fileno(outStream);
+      dup2(outputFile,1);
+      //should I close the file descriptor here? will it work?
+      if(outputFile != 0 && outputFile != 1 && outputFile != 2)
+        close(outputFile);
+    }
+
+    execvp(command->u.word[0], command->u.word);
+    error(1,0,"Failed to fork at simple command");
   }
+  else
+  {
+    if(waitpid(p,&status,0) < 0)
+      error(1,0,"WaitPid on Simple command failed!");
+    //printf("Executed Simple command\n");
+    command->status = WEXITSTATUS(status);
+  }  
+// must implement making u.word a char* array before this works properly (createSimpleCommand in read-command.c)
   return command->status;
 }
 
